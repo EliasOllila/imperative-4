a7f05717bdfe313edd7d3b3a1cd51cbef1b48930
eliasollila <elias.ollila@syk.fi>
2022-08-20 21:49:05 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+public class Main {
+
+    public static void main(String[] args) {
+        int T=0;
+        int n = (int)(Math.random() * 50);
+        System.out.println(n);
+    }
+}
